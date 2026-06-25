6527825d71400e025a217659ac6d3ed2bc252ac3
Miguel Barth <miguelbarth1@gmail.com>
2026-06-25 14:31:06 -0300

    Na1

--- Aula_06_AsType&Replace/01_ConvTipo.py
+++ Aula_06_AsType&Replace/01_ConvTipo.py
@@ -29,10 +29,58 @@ O dataset que baixamos já estava com este problema resolvido, por isso
 deixei apenas como exemplo para ficar claro.
 """
 
-df["DtCriacao"].replace(
+df["DtCriacao"] = df["DtCriacao"].replace(
     {
         "0000-00-00 00:00:00.000" : "2024-02-01 00:00:00.000"
     }
 )
 
 # %%
+"""
+Depois de tratarmos a data na célula anterior, conseguimos
+rodar o método 'pd.to_datetime()'.
+
+Assim transformamos o tipo de variável presente na serie de
+'object' para 'datetime64[ns]'.
+"""
+
+df["DtCriacao"] = pd.to_datetime(df["DtCriacao"])
+df["DtCriacao"]
+
+# %% Como é comum de ser feito no mercado:
+"""
+Aqui, escrevemos de forma resumida, clara e otimizada o que fizemos
+nas duas célular anteriores.
+
+1. Na variável 'replace' que criamos adicionamos todos os valores que
+queremos alterar.
+
+2. Aplicamos o replace (a alteração dos valores) dentro da função
+'pd.to_datetime'.
+
+3. Passamos o replace dentro da função para converter a variável para o
+formato de data.
+
+4. Reatribuímos em nossa serie de data.
+"""
+
+replace = {"0000-00-00 00:00:00.000" : "2024-02-01 00:00:00.000"}
+
+df["DtCriacao"] = pd.to_datetime(df["DtCriacao"].replace(replace))
+
+# %% Por que é interessante o '.to_datetime':
+
+df["DtCriacao"].dt.date() #Retorna só a data, sem a hora
+df["DtCriacao"].dt.day_of_week #Retorna o dia da semana
+df["DtCriacao"].dt.year() #Retorna só o ano
+df["DtCriacao"].dt.month() #Retorna só o número do mês
+df["DtCriacao"].dt.month_name() #Retorna só o nome do mês
+df["DtCriacao"].dt.day() #Retorna só o dia
+
+# Entre outros!!!
+
+"""
+Transformar a coluna para o tipo de 'datetime' faz com que seja possível
+utilizar o atributo '.dt', que nos permite trabalhar de uma forma muito
+simples com a data.
+"""
---
+++ Aula_07_Dados_Faltantes&Nulos/01_Na.py
@@ -0,0 +1,95 @@
+# %%
+
+import pandas as pd
+
+df = pd.read_csv("../data/clientes.csv", sep=";")
+df.head()
+
+# %% Replace dos dados para passar a função 'pd.to_datetime()'
+
+replace = {
+    "0000-00-00 00:00:00.000" : "2024-02-01 00:00:00.000"
+}
+
+df["DtCriacao"] = pd.to_datetime(df["DtCriacao"].replace(replace))
+df["DtCriacao"]
+
+# %% Primeira forma de lidar:
+"""
+Podemos simplesmente remover toda e qualquer linha que possua
+pelo menos um valor como 'Na' com o método '.dropna'
+"""
+
+df = df.dropna()
+df = df.dropna(how="any") #Igual ao de cima
+
+# %% Segunda forma
+"""
+Remove apenas as linhas que possuem TODOS os valores igual a Na.
+"""
+
+df = df.dropna(how="all") #Desconsidera somente as linhas que possuem todos os valores como NaN.
+
+# %% Exemplo com outro DataFrame
+
+brinquedo = pd.DataFrame(
+    {
+        "nome" : ["Miguel" , "Ana" , "José", None],
+        "idade" : [24 , None , 22, 29],
+        "salario" : [1200 , 2400 , None, 2400]
+    }
+)
+brinquedo
+# %% Testando diferentes formas de '.dropna()'
+"""
+Dropa as linhas com os nulos do dataframe baseando-se
+apenas na coluna 'nome'.
+"""
+
+brinquedo.dropna(how="all", subset="nome")
+
+# %%
+"""
+Dropa as linhas com os nulos do dataframe baseando-se
+nas colunas 'nome' e 'idade', tendo um valor nulo em algumas
+das colunas a linha já é dropada.
+"""
+
+brinquedo.dropna(how="any", subset=["nome", "idade"])
+
+# %% FillNa:
+"""
+Método usado para preencher as células que estão com valor nulo (Na)
+"""
+
+brinquedo["idade"] = brinquedo["idade"].fillna(0) #Substitui os valores nulos por 0
+brinquedo
+
+# %% Preenchendo diferentes colunas com diferentes valores
+
+brinquedo = brinquedo.fillna({
+    "nome" : "alguém",
+    "idade" : 0,
+    "salario" : 0
+    }
+)
+brinquedo
+
+# %% Preenchendo pela média da coluna
+"""
+Nesse caso, preenchendo pela média nós não estamos alterando a
+média dos dados preenchendo os nulos pela média.
+Estamos alterando o desvio padrão, do qual será reduzido.
+
+Ou seja, utilizando esse método em nossos dados estariamos
+subestimando nosso desvio padrão (bem como a variância e coeficiente
+de variação).+
+
+Há outras estratégias que podem ser mais interessantes, porém
+a depender do caso pode servir para nossa análise, em última
+instância.
+"""
+
+media = brinquedo[["idade", "salario"]].mean()
+brinquedo = brinquedo.fillna(media)
+brinquedo
